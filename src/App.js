e8905e970e9bf47abc55459d119350c8a060a0f7
amandaalmeida89 <amandaalmeida89@gmail.com>
2020-02-11 12:13:09 -0300

    firebase config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React {Route} from 'react';
+import React, { Route } from 'react';
 
 const App = () => {
 
   return (
     <>
-      <Route exact path="/" component={} />
-      <Route path="/register" component={} />
-      <Route path="/bartender" component={} />
-      <Route path="/delivery" component={} />
-      <Route path="/kitchen" component={} />
+      {/* <Route exact path="/" component={} /> */}
     </>
   );
 };
