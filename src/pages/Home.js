edfb939e1d289e165092803b63a386f61492b5fd
JessikaFujimura <jessikafujimura@gmail.com>
2020-02-11 15:08:02 -0300

    css do header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
+import Header from '../components/Header';
 
 const Home = () => {
   const [products, setProducts] = useState('');
   console.log(products)
 
-useEffect(()=> {
-  fetch('http://my-json-server.typicode.com/jusbrasil/hackathon-laboratoria/db')
-  .then(res => res.json())
-  .then((snap)=> {
-    const productsList = snap.products;
-    setProducts(productsList)
-  })
-},[])
+  useEffect(() => {
+    fetch('http://my-json-server.typicode.com/jusbrasil/hackathon-laboratoria/db')
+      .then(res => res.json())
+      .then((snap) => {
+        const productsList = snap.products;
+        setProducts(productsList)
+      })
+  }, [])
 
   return (
-    <div></div>
+    <div>
+      <Header
+        name="Teste"
+        handleClick={() => console.log('oi')}
+        id="button" />
+    </div>
   )
 
 }
